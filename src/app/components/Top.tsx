b7b264a1898d5beff10d54a88979c94c3776cd6b
Akimov Eugeney <akimoveugeney@gmail.com>
2025-04-08 18:19:19 +0300

    fix: img alt

--- src/app/components/Top.tsx
+++ src/app/components/Top.tsx
@@ -9,7 +9,7 @@ export default function Top() {
   return (
     <div className='absolute top-0 z-10 flex h-14 w-full justify-end rounded-br-2xl rounded-bl-2xl bg-fuchsia-300 p-2 opacity-80'>
       <div className='mr-3 flex items-center gap-2'>
-        <Image src={'/coin.svg'} width={40} height={40} />
+        <Image src={'/coin.svg'} width={40} height={40} alt='coin'/>
         <span className='text-4xl'>{score}</span>
       </div>
     </div>
